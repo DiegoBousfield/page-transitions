123a5e4054f304ae90c55d75297bef9c8b8798c8
Diego Bousfield <diegobousfield@ckl.io>
2020-10-07 17:34:17 -0300

    add between pages animations

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,48 @@
+const loadPage = async (url) =>
+  await fetch(url, { method: "GET" }).then((response) => response.text());
 
+const main = document.querySelector("main");
+const animate = (oldContent, newContent) => {
+  const fadeOut = oldContent.animate(
+    {
+      opacity: [1, 0],
+    },
+    500
+  );
+
+  fadeOut.onfinish = () => {
+    newContent.animate({ opacity: [0, 1] }, 500);
+    oldContent.parentNode.removeChild(oldContent);
+  };
+};
+
+const changePage = () => {
+  const url = window.location.href;
+
+  loadPage(url).then((responseText) => {
+    const wrapper = document.createElement("div");
+    wrapper.innerHTML = responseText;
+    const oldContent = document.querySelector(".content");
+    const newContent = wrapper.querySelector(".content");
+
+    main.appendChild(newContent);
+    animate(oldContent, newContent);
+  });
+};
+
+window.addEventListener("popstate", changePage);
+
+document.addEventListener("click", (e) => {
+  let el = e.target;
+
+  while (el && !el.href) {
+    el = el.parentNode;
+  }
+
+  if (el) {
+    e.preventDefault();
+    history.pushState(null, null, el.href);
+    changePage();
+    return;
+  }
+});
